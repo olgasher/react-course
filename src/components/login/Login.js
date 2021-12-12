aada17aa831137694038d4cc8a77be6156679956
olgasher <olga.sh2@gmail.com>
2021-12-12 15:17:27 +0200

    new example

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+
 export function Login() {
 
+    const [user, setUser] = useState(null)
+
     const loginSubmit= (e) => {
-        console.log("Hello");
+        setUser("")
         e.preventDefault();
+
+        //setUser ({firstName:'', lastName:'',email: ''})
     } 
 
     return (
         <form onSubmit={loginSubmit}>
-            <input name="user" type="email" type="text" placeholder="Enter your email: "/>
+            <input name="user" type="email" placeholder="Enter your email: "/>
             <input name="password" type="password" placeholder="Enter your password: "/>
             <button type="submit">Login</button>
+            <h1> hello {user}, you are now logged in </h1>
         </form>
     )
 }
